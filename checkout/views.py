1b178d62150b6b814fc9aa69ccd9c99d71e11c9d
Shane-Bath <>
2023-08-30 09:52:28 +0000

    added checkout views and template

--- checkout/views.py
+++ checkout/views.py
@@ -10,10 +10,10 @@ def checkout(request):
         messages.error(request, "There's nothing in your bag at the moment")
         return redirect(reverse('products'))
 
-    order_form = Orderform()
+    order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
-        'order_form': order_form
+        'order_form': order_form,
     }
 
     return render(request, template, context)
