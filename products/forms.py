e04ecfee5759053c388e3ffe4d133054783605c1
Shane-Bath <>
2023-09-13 21:04:05 +0000

    Product Admin - add product view, url and template

--- products/forms.py
+++ products/forms.py
@@ -10,10 +10,9 @@ class ProductForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        catergories = Catergory.objects.all()
+        catergories = Category.objects.all()
         friendly_names = [(c.id, c.get_friendly_name()) for c in catergories]
 
         self.fields['category'].choices = friendly_names
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'border-black rounded-0'
-            
