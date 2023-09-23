a4a525100825bec04e93b8a8b3ec7beccc1bc541
Shane-Bath <>
2023-09-23 08:39:38 +0000

    Add 404 page

---
+++ boutique_ado/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+
+def handler404(request, exception):
+    """Error Handler 404 - Page not found"""
+    return render(request, "errors/404.html", status=404)
+    
